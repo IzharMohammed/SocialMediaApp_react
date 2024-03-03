b600761c460119ce41eec78e4d92d22b0d4d71f9
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-03-04 00:53:26 +0530

    Added postCard component

---
+++ SocialMediaApp-react/src/components/SocialApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PostCard from './PostCard'
+function SocialApp() {
+  return (
+    <div>
+        <div>SocialApp</div>
+        <PostCard />
+    </div>
+  )
+}
+
+export default SocialApp
