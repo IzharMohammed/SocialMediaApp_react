46d63cdf85a3e931f51e6b5682ead2e37d3c6be1
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-03-04 23:24:30 +0530

    updated ui for basic main component

---
+++ SocialMediaApp-react/src/components/MainContainer/MainContainer.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Unstable_Grid2";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import PostCard from "../PostCard";
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+export default function RowAndColumnSpacing() {
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center" sx={{ width: "100%" }}>
+      <Grid container justifyContent={"center"} alignItems={"start"} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid  xs={0} md={1}>
+          <Item>1</Item>
+        </Grid>
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+          xs={12}
+          md={11}
+        >
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
--- SocialMediaApp-react/src/components/SocialApp.jsx
+++ SocialMediaApp-react/src/components/SocialApp.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PostCard from './PostCard'
 import Navbar from './Navbar/Navbar'
+import MainContainer from './MainContainer/MainContainer'
 function SocialApp() {
   return (
     <div>
        <Navbar />
-        <PostCard />
+    `     <MainContainer />
+
     </div>
   )
 }
