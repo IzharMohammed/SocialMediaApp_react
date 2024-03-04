18b1a2300610f9c1dce3a9d7d235bbf9b4048293
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-03-05 02:59:01 +0530

    Added users list from dummy api

--- SocialMediaApp-react/src/components/MainContainer/MainContainer.jsx
+++ SocialMediaApp-react/src/components/MainContainer/MainContainer.jsx
@@ -3,8 +3,8 @@ import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Unstable_Grid2";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import PostCard from "../PostCard";
 import PostCardList from "../PostCardList/PostCardList";
+import UserList from "../UserList/UserList"
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -15,18 +15,17 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function RowAndColumnSpacing() {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" sx={{ width: "100%" }}>
+    <Box display="flex" justifyContent="space-around" alignItems="center" sx={{ width: "100%" }}>
       <Grid container justifyContent={"center"} alignItems={"start"} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid  xs={0} md={1}>
-          <Item>1</Item>
+        <Grid   md={6}>
+         <UserList />
         </Grid>
         <Grid
           container
           justifyContent="center"
           alignItems="center"
           direction="column"
-          xs={12}
-          md={11}
+          md={6}
         >
        <PostCardList />
         </Grid>
